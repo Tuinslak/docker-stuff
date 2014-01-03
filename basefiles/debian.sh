0f487a219d2a0274d66cbc0a50896ac6e916120e
Yeri Tiete <yeri@flatturtle.com>
2014-01-03 11:31:20 +0100

    added cron in readme, fixed typo

--- basefiles/debian.sh
+++ basefiles/debian.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+
+# This is what my cron looks like:
+#	# m h  dom mon dow   command
+#	30 4 * * 3	cd /home/docker/debian/ && su root -c /home/docker/debian/bootstrap.sh >/dev/null 2>&1
+# be sure to add the "su root -c" part, or mounting in chroot will fail.
+
 # change path; might give problems if you don't change this and when you use cron
 cd /home/docker/debian/
 
 # bootstrap
-/usr/sbin/debootstradebootstrap wheezy wheezy
+/usr/sbin/debootstrap wheezy wheezy
 PID=$(tar -C wheezy/ -c . | docker import - wheezy)
 
 # docker stuff
